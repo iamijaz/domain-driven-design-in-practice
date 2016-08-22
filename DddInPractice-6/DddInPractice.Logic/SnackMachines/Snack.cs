eb35df58dec271c47d12a8777122562712ba9ca1
Ijaz Ahmed <Ijaz.Ahmed@dunnhumby.com>
2016-08-22 16:17:04 +0100

    fixed the namespaces

--- DddInPractice-6/DddInPractice.Logic/SnackMachines/Snack.cs
+++ DddInPractice-6/DddInPractice.Logic/SnackMachines/Snack.cs
@@ -2,16 +2,16 @@
 
 namespace DddInPractice.Logic.SnackMachines
 {
-    public class Snack : AggregateRoot
+    public sealed class Snack : AggregateRoot
     {
         public static readonly Snack None = new Snack(0, "None");
         public static readonly Snack Chocolate = new Snack(1, "Chocolate");
         public static readonly Snack Soda = new Snack(2, "Soda");
         public static readonly Snack Gum = new Snack(3, "Gum");
 
-        public virtual string Name { get; }
+        public string Name { get; }
 
-        protected Snack()
+        private Snack()
         {
         }
 
--- DddInPractice-6/DddInPractice.UI/App.xaml.cs
+++ DddInPractice-6/DddInPractice.UI/App.xaml.cs
@@ -1,12 +1,16 @@
-﻿using DddInPractice.Logic.Utils;
+﻿
 
-namespace DddInPractice.Logic.UI
+
+
+using DddInPractice.Logic.Utils;
+
+namespace DddInPractice.UI
 {
     public partial class App
     {
         public App()
         {
-            Initer.Init(@"Server=.;Database=DddInPractice;Trusted_Connection=true");
+            Initer.Init(@"Server=.;Database=DddInPractice;User Id=HttpProxy;Password=Password123;");
         }
     }
 }
